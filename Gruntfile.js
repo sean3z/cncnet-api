eb341e64870213987e2b609b762b0dab595cb567
Tahj <tahj.kirk@gmail.com>
2015-02-27 00:36:44 -0500

    adding copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,17 @@ module.exports = function(grunt) {
     grunt.initConfig({
         clean: ['dist'],
         
+        copy: {
+            development: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/www',
+                    src: ['**', '!**/less/**'],
+                    dest: 'dist/www/'
+                }]
+            }
+        },
+        
         less: {
             development: {
                 files: [{
@@ -44,11 +55,12 @@ module.exports = function(grunt) {
     });
     
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-concurrent');
     
-    grunt.registerTask('serve-app-dev', ['clean', 'less:development', 'concurrent:development']);
-    grunt.registerTask('build-app-prod', ['clean', 'less:development']);
+    grunt.registerTask('serve-app-dev', ['clean', 'copy', 'less:development', 'concurrent:development']);
+    grunt.registerTask('build-app-prod', ['clean', 'copy', 'less:development']);
 };
