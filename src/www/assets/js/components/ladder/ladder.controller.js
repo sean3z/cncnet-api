24f74c96d9f7213e47573effc11d8b3dfb136778
Tahj <tahj.kirk@gmail.com>
2015-03-01 13:44:31 -0500

    using response from ladder service

--- src/www/assets/js/components/ladder/ladder.controller.js
+++ src/www/assets/js/components/ladder/ladder.controller.js
@@ -2,10 +2,12 @@ angular.module('Ladder').controller('LadderCtrl', LadderCtrl);
 
 LadderCtrl.$inject = ['$state', '$stateParams', 'LadderSvc'];
 function LadderCtrl($state, $stateParams, LadderSvc) {
+    var vm = this;
+    
     console.log('$state', $state);
     console.log('Ladder', $stateParams);
 
     LadderSvc.top($stateParams.game).then(function(response) {
-        console.log('response', response);
+        vm.players = response;
     });
 }
