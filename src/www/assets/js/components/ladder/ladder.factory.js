191e5c3a3ae65f45ae99552d7bd636fea1bf9c7e
Tahj <tahj.kirk@gmail.com>
2015-03-01 13:57:15 -0500

    jshint fix

--- src/www/assets/js/components/ladder/ladder.factory.js
+++ src/www/assets/js/components/ladder/ladder.factory.js
@@ -3,7 +3,7 @@ angular.module('Ladder').factory('LadderSvc', LadderSvc);
 LadderSvc.$inject = ['$http', '$q'];
 function LadderSvc($http, $q) {
     var factory = {};
-    var url = window.location.protocol +'//'+ window.location.hostname + ':4007/ladder';
+    var url = window.location.protocol + '//' + window.location.hostname + ':4007/ladder';
 
     factory.top = function(game) {
         var deferred = $q.defer();
