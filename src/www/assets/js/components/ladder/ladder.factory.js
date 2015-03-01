938c3bdd01a1412ccfa7f43b5cd98375427b6013
Tahj <tahj.kirk@gmail.com>
2015-03-01 13:44:06 -0500

    adding protocol, otherwise call fails due to different port

--- src/www/assets/js/components/ladder/ladder.factory.js
+++ src/www/assets/js/components/ladder/ladder.factory.js
@@ -3,13 +3,13 @@ angular.module('Ladder').factory('LadderSvc', LadderSvc);
 LadderSvc.$inject = ['$http', '$q'];
 function LadderSvc($http, $q) {
     var factory = {};
-    var url = window.location.hostname + ':4007/ladder';
+    var url = window.location.protocol +'//'+ window.location.hostname + ':4007/ladder';
 
     factory.top = function(game) {
         var deferred = $q.defer();
 
         $http.get(url + '/' + game).then(function(results) {
-            deferred.resolve(results);
+            deferred.resolve(results.data);
         });
 
         return deferred.promise;
