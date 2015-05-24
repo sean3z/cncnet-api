c36397e3e96b176318a7cab3dbfd766c1b9ea78e
Tahj <tahj.kirk@gmail.com>
2015-05-24 17:11:53 -0400

    adding basicAuth example

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,3 +1,17 @@
+var basicAuth = require('basic-auth');
+
 exports.player = function(req, res, next) {
+    var credentials = basicAuth(req);
+    
+    console.log(credentials);
+    
+    if (!credentials || credentials.name !== 'john' || credentials.pass !== 'secret') {
+        res.writeHead(401, {
+            'WWW-Authenticate': 'Basic realm="example"'
+        });
+        res.end();
+    } else {
+        res.json({test: 'meow'});
+    }
     
 };
