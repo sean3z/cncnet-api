3fa002ab7a914397c26aba00090214a084683aa7
Tahj <tahj.kirk@gmail.com>
2015-05-26 22:37:32 -0400

    ensure all game params are legit

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var WOL_PORT = process.env.WOL_PORT || 4007;
 var bodyParser = require('body-parser');
+var WOL_PORT = process.env.WOL_PORT || 4007;
 
 /* route separation http://bit.ly/1Kt87xZ */
 var ping = require('./routes/ping');
@@ -11,15 +11,28 @@ var auth = require('./routes/auth');
 app.use(bodyParser.raw());
 app.use(express.static(__dirname + '/../client'));
 
-// General
+/* ensure game abbr is valid */
+app.param('game', function(req, res, next, abbr) {
+    if (!(!!abbr.match(/^(td|d2k?|ra2?|ts|fs|yr)$/))) {
+        return next(new Error('game abbr invalid'));
+    }
+    next();
+});
+
+/* general */
 app.get('/ping', ping);
 
-// Ladder
+/* ladder */
 app.post('/ladder/:game', ladder.game);
 
-// Auth
+/* auth */
 app.get('/auth/:game/:player', auth.player);
 app.put('/auth/:game/:player', auth.create);
+
+/* error handler */
+app.use(function(err, req, res, next) {
+    res.status(500).json({error: err.message});
+});
 
 app.listen(WOL_PORT, function() {
     console.log('WOL Ladder listening on %s:%s', require('os').hostname(), WOL_PORT);
