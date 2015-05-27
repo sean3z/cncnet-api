cfa99e207a70f7c9c513d214c553df334de5c5c3
Tahj <tahj.kirk@gmail.com>
2015-05-26 23:32:26 -0400

    no need to export entire object when only using one method

--- src/server/lib/gameres.js
+++ src/server/lib/gameres.js
@@ -1,123 +1,121 @@
-// WOL Game Resolution interpreter
-var GameResolution = {
-	parse: function (packet) {
+/* WOL field type interpreter */
+function _type(type, chunk) {
+    var data = 'Unprocessed Type';
 
-		var buffer = packet;
-		if (typeof packet === 'string') {
-			// remove any unnessiccary whitespace
-			packet = packet.replace(/(\r|\n|\r\n|\s+)/gm, '');
-			buffer = new Buffer(packet, 'hex');
-		}
+    switch (type) {
+        case 1:
+            // [15:39:10] <CCHyper> 1 = byte
+            data = chunk.readInt8(0);
+        break;
 
-		var slice = buffer.slice(0, 4);
-        var bufferLength = slice.readUInt16BE(0);
-        var flat = {}, i = 4;
+        case 2:
+            // [15:39:24] <CCHyper> 2 = boolean
+            data = chunk.readInt8(0) > 0 ? 1 : 0;
+        break;
 
-		while (i < bufferLength) {
-			var chunk = buffer.slice(i,  i + 4);
-            var field = chunk.toString();
+        case 3:
+            // [15:38:14] <CCHyper> 3 = short
+            data = chunk.readInt16BE(0);
+        break;
 
-			i += 4;
+        case 4:
+            // [15:38:07] <CCHyper> 4 = unsigned short
+            data = chunk.readUInt16BE(0);
+        break;
 
-			chunk = buffer.slice(i, i + 8);
-				
-            var type = chunk.readUInt16BE(0);
-            var length = chunk.readUInt16BE(2);
-            var data = 'Unprocessed Data';
+        case 5:
+            // [15:37:56] <CCHyper> 5 = long
+            data = chunk.readInt32BE(0);
+        break;
 
-			i += 4;
+        case 6:
+            // [15:37:49] <CCHyper> 6 = ulong/ unsigned long
+            data = chunk.readUInt32BE(0);
+        break;
 
-            var end = i + length >= bufferLength ? bufferLength : i + length;
+        case 7:
+            // [15:38:35] <CCHyper> 7= char
+            data = chunk.toString('ascii').replace('\u0000', ''); // charCode(0) at end of string
+            if (data == "ON") data = 1;
+            else if (data == "OFF") data = 0;
+        break;
 
-            if (i <= end) {
-                data = this.type(type, buffer.slice(i, end));
-                length = Math.ceil(length / 4) * 4;
-                i += length;
+        case 20:
+            // [15:39:34] <CCHyper> 20 is a string
+            // [15:40:28] <CCHyper> 0x14 is qword
+            var _data = 0, x = -1;
+            for (var i = 0; i < chunk.length; i += 4) {
+                var end = i + 4 >= chunk.length ? chunk.length : i + 4;
+                var slice = chunk.slice(i, end);
+                _data += slice.readUInt32BE(0);
             }
-			
-			flat[field] = data;
-		}
+            data = _data;
+        break;
+    }
 
-		return this.consolidate(flat);
-	},
+    return data;
+}
 
-	/* WOL field type interpreter */
-	type: function (type, chunk) {
-		var data = 'Unprocessed Type';
+/* consolidate player stats */
+function _consolidate(flat) {
+    var consolidated = {players: {}};
 
-		switch (type) {
-            case 1:
-                // [15:39:10] <CCHyper> 1 = byte
-                data = chunk.readInt8(0);
-            break;
-                
-			case 2:
-				// [15:39:24] <CCHyper> 2 = boolean
-				data = chunk.readInt8(0) > 0 ? 1 : 0;
-			break;
-                
-            case 3:
-                // [15:38:14] <CCHyper> 3 = short
-                data = chunk.readInt16BE(0);
-            break;
-                
-            case 4:
-                // [15:38:07] <CCHyper> 4 = unsigned short
-                data = chunk.readUInt16BE(0);
-            break;
+    if (flat.NAM0 || flat.NAM1) {
+        for (var key in flat) {
+            var val = flat[key], index = parseInt(key.slice(-1));
 
-			case 5:
-                // [15:37:56] <CCHyper> 5 = long
-				data = chunk.readInt32BE(0);
-			break;
-                
-            case 6:
-                // [15:37:49] <CCHyper> 6 = ulong/ unsigned long
-                data = chunk.readUInt32BE(0);
-            break;
+            if (index > -1) {
+                if (!consolidated.players[index]) consolidated.players[index] = {};
+                consolidated.players[index][key.slice(0, -1)] = val;
+            } else {
+                consolidated[key] = val;
+            }
+        }
+    }
 
-			case 7:
-				// [15:38:35] <CCHyper> 7= char
-				data = chunk.toString('ascii').replace('\u0000', ''); // charCode(0) at end of string
-				if (data == "ON") data = 1;
-				else if (data == "OFF") data = 0;
-			break;
-                
-			case 20:
-				// [15:39:34] <CCHyper> 20 is a string
-				// [15:40:28] <CCHyper> 0x14 is qword
-				var _data = 0, x = -1;
-				for (var i = 0; i < chunk.length; i += 4) {
-					var end = i + 4 >= chunk.length ? chunk.length : i + 4;
-					var slice = chunk.slice(i, end);
-					_data += slice.readUInt32BE(0);
-				}
-				data = _data;
-			break;
-		}
+    return consolidated;
+}
 
-		return data;
-	},
-    
-    /* consolidate player stats */
-	consolidate: function (flat) {
-		var consolidated = {players: {}};
-		
-		if (flat.NAM0 || flat.NAM1) {
-			for (var key in flat) {
-				var val = flat[key], index = parseInt(key.slice(-1));
+/* WOL Game Resolution interpreter */
+function _parse(packet) {
 
-				if (index > -1) {
-					if (!consolidated.players[index]) consolidated.players[index] = {};
-					consolidated.players[index][key.slice(0, -1)] = val;
-				} else {
-					consolidated[key] = val;
-				}
-			}
-		}
+    var buffer = packet;
+    if (typeof packet === 'string') {
+        // remove any unnessiccary whitespace
+        packet = packet.replace(/(\r|\n|\r\n|\s+)/gm, '');
+        buffer = new Buffer(packet, 'hex');
+    }
 
-		return consolidated;
-	}
-};
+    var slice = buffer.slice(0, 4);
+    var bufferLength = slice.readUInt16BE(0);
+    var flat = {}, i = 4;
 
-module.exports = GameResolution;
+    while (i < bufferLength) {
+        var chunk = buffer.slice(i,  i + 4);
+        var field = chunk.toString();
+
+        i += 4;
+
+        chunk = buffer.slice(i, i + 8);
+
+        var type = chunk.readUInt16BE(0);
+        var length = chunk.readUInt16BE(2);
+        var data = 'Unprocessed Data';
+
+        i += 4;
+
+        var end = i + length >= bufferLength ? bufferLength : i + length;
+
+        if (i <= end) {
+            data = _type(type, buffer.slice(i, end));
+            length = Math.ceil(length / 4) * 4;
+            i += length;
+        }
+
+        flat[field] = data;
+    }
+
+    return _consolidate(flat);
+}
+
+module.exports = _parse;
