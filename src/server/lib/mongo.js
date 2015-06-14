0e507f73cfb73cd502e6d0347d24f515b12f6f77
Sean Wragg <Sean.Wragg@disney.com>
2015-06-14 09:17:53 -0400

    using monk

--- src/server/lib/mongo.js
+++ src/server/lib/mongo.js
@@ -1,10 +1,1 @@
-var debug = require('debug')('leaderboard');
-var Client = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/leaderboard';
-
-Client.connect(url, function(err, db) {
-    if (err) throw err;
-    debug('Connected to MongoDB');
-});
-
-module.exports = Client;
+module.exports = require('monk')('localhost/leaderboard');
