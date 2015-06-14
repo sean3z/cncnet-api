e286d4f455c46cf7ca6aa973176661937ff009ec
Sean Wragg <Sean.Wragg@disney.com>
2015-06-14 10:08:16 -0400

    adding debug statements and todo

--- src/server/lib/match.js
+++ src/server/lib/match.js
@@ -1,15 +1,19 @@
 /* collection of game related helper functions */
 var $db = require('./mongo');
+var debug = require('debug')('wol:leaderboard');
 var gameres = require('./gameres');
 var $q = require('q');
 
 exports.process = function(game, dmp) {
     var defer = $q.defer();
     var match = gameres(dmp);
+    debug('WOL Gameres packet recieved');
+    debug('game: %s, idno: %d, player: %s', game, match.idno, match.players[match.client.spid].nam);
 
     defer.resolve(match);
 
     // create raw dump entry
+    // TODO: check against spid (sender id) to ensure only 1 packet from each player
     var buffer = match.buffer.toString('utf8');
     $db.get(game +'_dumps').update(
         {idno: match.idno},
