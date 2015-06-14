d9489605eaca75b7fa89bb88e0ba7d6fd18a5c3a
GrantBartlett <hello@grant-bartlett.com>
2015-06-14 14:01:23 +0100

    Some restructuring and table styling

--- src/public/modules/leaderboard/controllers/leaderboard.client.controller.js
+++ src/public/modules/leaderboard/controllers/leaderboard.client.controller.js
@@ -6,17 +6,33 @@ angular.module('Leaderboard').controller('LeaderboardController', ['$scope', '$s
 
         console.log('$state', $state);
         console.log('Leaderboard', $stateParams);
-        console.log($scope);
 
         LadderSvc.top($stateParams.game).then(function(response) {
            $scope.players = response;
         });
 
-        // Test Data to play with UI
-        $scope.players = [
-            {name: 'Grant', win_count: 50, loss_count: 25, points: 250},
-            {name: 'Sean', win_count: 10, loss_count: 55, points: 150}
-        ];
+        $scope.limit = 10;
 
+        // Test Data to play with UI
+        $scope.players =
+        [
+            {name: 'Grant', win_count: 50, loss_count: 25, points: 355},
+            {name: 'tahj', win_count: 10, loss_count: 55, points: 21},
+            {name: 'funkyfr3sh', win_count: 50, loss_count: 25, points: 3},
+            {name: 'iran', win_count: 215, loss_count: 25, points: 250},
+            {name: 'hifi', win_count: 55, loss_count: 25, points: 5},
+            {name: 'cchyper', win_count: 50, loss_count: 25, points: 2},
+            {name: 'myg', win_count: 25, loss_count: 25, points: 3},
+            {name: 'tore', win_count: 50, loss_count: 25, points: 55},
+            {name: 'rockers', win_count: 3, loss_count: 25, points: 5},
+            {name: 'lolipopladyman', win_count: 50, loss_count: 25, points: 35},
+            {name: 'rocking', win_count: 50, loss_count: 25, points: 45},
+            {name: 'crackers', win_count: 50, loss_count: 25, points: 26},
+            {name: 'badstuff', win_count: 50, loss_count: 25, points: 27},
+            {name: 'statwhore', win_count: 50, loss_count: 25, points: 16},
+            {name: 'loveW1nning', win_count: 50, loss_count: 25, points: 16},
+            {name: 'ojset1o5j', win_count: 50, loss_count: 25, points: 6},
+            {name: 'theRock24', win_count: 10, loss_count: 55, points: 3}
+        ]
     }
 ]);
