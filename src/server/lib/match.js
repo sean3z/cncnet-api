6bf6e8a2b594f34437aa2f8c2fdab772f16feea2
Sean Wragg <Sean.Wragg@disney.com>
2015-06-14 16:56:35 -0400

    enter player data

--- src/server/lib/match.js
+++ src/server/lib/match.js
@@ -14,21 +14,43 @@ exports.process = function(game, dmp) {
 
     // create raw dump entry
     // TODO: check against spid (sender id) to ensure only 1 packet from each player
-    var buffer = match.buffer.toString('utf8');
-    $db.get(game +'_dumps').update(
+    var $dumps = $db.get(game +'_dumps');
+    $dumps.update(
         {idno: match.idno},
-        {$push: {buffer: buffer}},
+        {$push: {buffers: match.buffer.toString('utf8')}},
         {upsert: true}
     );
 
-    // create player entry
-    match.players.forEach(function(player) {
-        // locate player; insert or update
-    });
+    // todo: make this more efficent; no need to update then find
+    // should be able to use the same search query
+    $dumps.find({idno: match.idno}, function(err, doc) {
+        if (doc.length > 1) {
+            // error scneario, we have two entries for the same game
+            // todo: figure out what the hell to do?
+            // todo: elimiate duplicates?
+            return;
+        }
 
-    // create game entry
-    // mongo.insert(game +'_'+ 'games', dmp);
-    // locate ts_dumps document with matching idno then insert or update
+        // only continue if this is the first entry for a game
+        if (doc[0].buffers.length > 1) return;
+
+        // create player entry
+        var $players = $db.get(game +'_players');
+        match.players.forEach(function(player) {
+            $players.update(
+                {name: player.nam},
+                {
+                    $push: {games: match.idno},
+                    $inc: {points: 2}
+                },
+                {upsert: true}
+            );
+        });
+
+        // create game entry
+        // mongo.insert(game +'_'+ 'games', dmp);
+        // locate ts_games document with matching idno then insert or update
+    });
 
     return defer.promise;
 };
