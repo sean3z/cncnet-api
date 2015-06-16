acdbde0a2389c21f71b4c210ac91bc31da967d17
GrantBartlett <hello@grant-bartlett.com>
2015-06-16 21:47:24 +0100

    removing test modal material from controller

--- src/public/modules/leaderboard-player/controllers/leaderboard-player.client.controller.js
+++ src/public/modules/leaderboard-player/controllers/leaderboard-player.client.controller.js
@@ -16,49 +16,6 @@ angular.module('Leaderboard.Player').controller('LeaderboardPlayerController', [
             losses: 10,
             points: 250
         };
-
-        $scope.data =  [
-            ['Firefox', 45.0],
-            ['IE', 26.8],
-            ['Chrome', 12.8],
-            ['Safari', 8.5],
-            ['Opera', 6.2],
-            {
-                name: 'Others',
-                y: 0.7,
-                dataLabels: {
-                    enabled: false
-                }
-            }
-        ];
-        $scope.items = ['item1', 'item2', 'item3'];
-
-        $scope.animationsEnabled = true;
-
-        $scope.open = function (size) {
-
-            var modalInstance = $modal.open({
-                animation: $scope.animationsEnabled,
-                templateUrl: 'modules/leaderboard-player/views/test.html',
-                size: size,
-                resolve: {
-                    items: function () {
-                        return $scope.items;
-                    }
-                }
-            });
-
-            modalInstance.result.then(function (selectedItem) {
-                $scope.selected = selectedItem;
-            }, function () {
-                $log.info('Modal dismissed at: ' + new Date());
-            });
-        };
-
-        $scope.toggleAnimation = function () {
-            $scope.animationsEnabled = !$scope.animationsEnabled;
-        };
-
     }
 ]);
 
