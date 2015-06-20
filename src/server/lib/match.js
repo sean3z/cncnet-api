4a8c50f9e89c43b5c2e88a7ec2de0b6b5ce6a19a
Tahj <tahj.kirk@gmail.com>
2015-06-20 17:36:25 -0400

    error handling

--- src/server/lib/match.js
+++ src/server/lib/match.js
@@ -6,8 +6,8 @@ var $q = require('q');
 
 exports.process = function(game, dmp) {
     var match = gameres(dmp);
-    debug('WOL Gameres packet recieved');
-    debug('game: %s, idno: %d, player: %s', game, match.idno, match.players[match.client.spid].nam);
+    if (!match.idno) return; /* if no game id, discontinue */
+    console.log('game: %s, idno: %d', game, match.idno);
 
     // create raw dump entry
     // TODO: check against spid (sender id) to ensure only 1 packet from each player
@@ -15,48 +15,58 @@ exports.process = function(game, dmp) {
     $dumps.update(
         {idno: match.idno},
         {$push: {buffers: match.buffer.toString('utf8')}},
-        {upsert: true}
+        {upsert: true},
+        _stats
     );
 
     // todo: make this more efficent; no need to update then find
     // should be able to use the same search query
-    $dumps.find({idno: match.idno}, function(err, doc) {
-        if (doc.length > 1) {
-            // error scneario, we have two entries for the same game
-            // todo: figure out what the hell to do?
-            // todo: elimiate duplicates?
-            return;
-        }
-
-        // only continue if this is the first entry for a game
-        if (doc[0].buffers.length > 1) return;
-
-        // create player entry
-        var $players = $db.get(game +'_players');
-        match.players.forEach(function(player) {
-            var stats = {
-                $push: {games: match.idno},
-                $inc: {points: 2}
-            };
-
-            /* evaluate wolv2 completions */
-            // 256 is won, 512 is defated
-            // 528 is lost connection or kicked
-            if (player.cmp) {
-                stats.$inc[(player.cmp == 256 ? 'wins' : 'losses')] = 1;
-                if (player.cmp == 528) stats.$inc.disconnects = 1;
-                if (player.cmp == 256) stats.$inc.points += 10;
+    function _stats(err, data) {
+        $dumps.find({idno: match.idno}, function(err, doc) {
+            if (doc.length > 1) {
+                // error scneario, we have two entries for the same game
+                // todo: figure out what the hell to do?
+                // todo: elimiate duplicates?
+                console.log('found duplicates game: %s, idno: %d', game, match.idno);
+                return;
             }
 
-            $players.update({name: player.nam}, stats, {upsert: true});
-        });
+            // only continue if this is the first entry for a game
+            if (doc[0].buffers && doc[0].buffers.length > 1) return;
 
-        // create game entry if WOLv2
-        if (match.client.vers.toLowerCase() === 'v2.0') {
-            // mongo.insert(game +'_'+ 'games', dmp);
-            // locate ts_games document with matching idno then insert or update
-        }
-    });
+            // create player entry
+            var $players = $db.get(game +'_players');
+            match.players.forEach(function(player) {
+                /* typically Computer */
+                if (!player.nam) {
+                    player.nam = 'Computer';
+                    return;
+                }
+
+                var stats = {
+                    $push: {games: match.idno},
+                    $inc: {points: 2}
+                };
+
+                /* evaluate wolv2 completions */
+                // 256 is won, 512 is defated
+                // 528 is lost connection or kicked
+                if (player.cmp) {
+                    stats.$inc[(player.cmp == 256 ? 'wins' : 'losses')] = 1;
+                    if (player.cmp == 528) stats.$inc.disconnects = 1;
+                    if (player.cmp == 256) stats.$inc.points += 10;
+                }
+
+                $players.update({name: player.nam}, stats, {upsert: true});
+            });
+
+            // create game entry if WOLv2
+            if (match.client.vers.toLowerCase() === 'v2.0') {
+                // mongo.insert(game +'_'+ 'games', dmp);
+                // locate ts_games document with matching idno then insert or update
+            }
+        });
+    }
 
     return;
 };
