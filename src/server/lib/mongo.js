4757b3411bcb5551a06d3d99fddc615d98b10c32
Sean Wragg <Sean.Wragg@disney.com>
2015-06-20 11:55:51 -0400

    adding indexes

--- src/server/lib/mongo.js
+++ src/server/lib/mongo.js
@@ -1,1 +1,5 @@
-module.exports = require('monk')('localhost/leaderboard');
+var $db = require('monk')('localhost/leaderboard');
+$db.get('ts_dumps').index('idno', {unique: true});
+$db.get('ts_players').index('name', {unique: true});
+$db.get('ts_games').index('idno', {unique: true});
+module.exports = $db;
