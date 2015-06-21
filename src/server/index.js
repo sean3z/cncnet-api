64d189a1cf5698f13df77f556e67d83ce26ccb43
Tahj <tahj.kirk@gmail.com>
2015-06-21 10:07:04 -0400

    adding static server back

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,7 @@
-var express = require('express');
 var restify = require('restify');
 var app = restify.createServer();
 var path = require('path');
 var WOL_PORT = process.env.WOL_PORT || 4007;
-
-app.use(restify.queryParser());
-app.use(restify.jsonp());
 
 /* route separation http://bit.ly/1Kt87xZ */
 var ping = require('./routes/ping');
@@ -13,10 +9,10 @@ var leaderboard = require('./routes/leaderboard');
 var auth = require('./routes/auth');
 var debug = require('./routes/debug');
 
+app.use(restify.queryParser());
+app.use(restify.jsonp());
 app.use(restify.bodyParser());
 /*
-app.use(express.static(path.join(__dirname, '/../../dist/public')));
-
 // ensure game abbr is valid
 app.param('game', function(req, res, next, abbr) {
     if (!(!!abbr.match(/^(td|d2k?|ra2?|ts|fs|yr)$/))) {
@@ -37,10 +33,11 @@ app.get('/ladder/:game', leaderboard.rankings);
 app.get('/auth/:game/:player', auth.player);
 app.put('/auth/:game/:player', auth.create);
 
-/* error handler */
-app.use(function(err, req, res, next) {
-    res.status(500).json({error: err.message, stack: err.stack});
-});
+/* static server for development */
+app.get(/.*/, restify.serveStatic({
+    directory: __dirname + '/../../dist/public',
+    default: 'index.html'
+}));
 
 app.listen(WOL_PORT, function() {
     console.log('WOL Leaderboard listening on %s:%s', require('os').hostname(), WOL_PORT);
