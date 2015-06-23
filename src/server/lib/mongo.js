fc98fdc662f71f46deb4c9e8e53e378667110b79
Tahj <tahj.kirk@gmail.com>
2015-06-22 20:09:27 -0400

    adding ra indexes

--- src/server/lib/mongo.js
+++ src/server/lib/mongo.js
@@ -2,4 +2,9 @@ var $db = require('monk')('localhost/leaderboard');
 $db.get('ts_dumps').index('idno', {unique: true});
 $db.get('ts_players').index('name', {unique: true});
 $db.get('ts_games').index('idno', {unique: true});
+
+$db.get('ra_dumps').index('idno', {unique: true});
+$db.get('ra_players').index('name', {unique: true});
+$db.get('ra_games').index('idno', {unique: true});
+
 module.exports = $db;
