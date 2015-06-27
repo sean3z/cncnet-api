3fbd032431bdbcd268e2d595f5a0e1cb2359f8fd
Sean Wragg <seanwragg@gmail.com>
2015-06-27 13:34:12 -0400

    adding team object and additional information

--- src/server/WOL/v2.js
+++ src/server/WOL/v2.js
@@ -5,6 +5,7 @@ exports.process = function(game, match) {
     // only continue if we have proper stats
     // TODO: figure out why cmp is 2 or 8
     if ([2, 8].indexOf(match.players[0].cmp) > -1) return;
+    match.teams = [];
 
     // create player entry
     var $players = $db.get(game +'_players');
@@ -30,15 +31,25 @@ exports.process = function(game, match) {
             if (player.cmp == 528) stats.$inc.disconnects = 1;
             if (player.cmp == 256) stats.$inc.points += 4;
         }
+        
+        /* remove unused info */
+        delete player.ipa; /* could be tunnel or p2p (unreliable) */
+        delete player.ser; /* serials are not used on CnCNet */
 
         $players.update({name: player.nam}, stats, {upsert: true});
+        
+        /* associate player by teamId */
+        match.teams[player.tid] = match.teams[player.id] || [];
+        match.teams[player.tid].push(player.nam);
 
         /* tack on stats so it can be referenced in game object */
         player.__gained = stats.$inc;
     });
 
     // create game entry
-    delete match.buffer;
+    delete match.buffer; /* only used for additional parsing */
+    delete match.client; /* ununsed information about the client */
+    
     $db.get(game +'_games').insert(match);
 
     /* handle any game specific processing */
