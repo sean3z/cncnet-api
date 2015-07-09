6c57d7a434e0a10f36d78357564979af7bfcb8f8
Sean Wragg <Sean.Wragg@disney.com>
2015-07-09 17:35:15 -0400

    limiting leaderboard to top 1000 and ensuring cache build doesn't hinder response

--- src/server/lib/ranking.js
+++ src/server/lib/ranking.js
@@ -6,36 +6,30 @@ last_update['ra'] = _timestamp() - 350;
 
 exports.player = function(game, limit) {
     var defer = $q.defer();
-    _notch(game).then(function() {
-        $db.get(game + '_ladder').find({}, {limit: limit, sort: {rank: 1}}, function(err, data) {
-            // data.push({last_update: last_update[game]});
-            defer.resolve(data);
-        });
+
+    $db.get(game + '_ladder').find({}, {limit: limit, sort: {rank: 1}}, function(err, data) {
+        // data.push({last_update: last_update[game]});
+        // TODO: remove games array from each player in result
+        defer.resolve(data);
+
+        /* if cache theshold elapsed; generate new cache*/
+        if (last_update[game] < _timestamp() - 300) _notch(game);
     });
 
     return defer.promise;
 };
 
+/* updates leaderboard cache */
 function _notch(game) {
-    var defer = $q.defer();
-
-    if (last_update[game] > _timestamp() - 300) {
-        defer.resolve();
-        return defer.promise;
-    }
-    console.log('updated');
-    $db.get(game + '_players').find({}, {sort: {points: -1}}, function(err, data) {
+    $db.get(game + '_players').find({}, {limit: 1000, sort: {points: -1}}, function(err, data) {
         data.forEach(function(item, index) {
             item.rank = (index + 1);
         });
         $db.get(game + '_ladder').drop();
         $db.get(game + '_ladder').insert(data, function(err, doc) {
             last_update[game] = _timestamp();
-            defer.resolve();
         });
     });
-
-    return defer.promise;
 }
 
 function _timestamp() {
