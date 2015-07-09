a1cf2ccacd1d5b2fb8115b1ae119dc039c6dd85c
Sean Wragg <Sean.Wragg@disney.com>
2015-07-09 17:34:18 -0400

    adding support for foreign characters and limiting games list to 20

--- src/server/lib/player.js
+++ src/server/lib/player.js
@@ -1,31 +1,35 @@
 var $db = require('./mongo');
 var $q = require('q');
 
-/* method to search for a quick list of players */
+/* method to search for a quick list of players by name */
 exports.locate = function(game, search) {
     var defer = $q.defer();
-    $db.get(game + '_players').find({name: new RegExp(search, 'i')}, function(err, data) {
+    $db.get(game + '_players').find({name: _sanitize(search)}, function(err, data) {
+        // TODO: remove games array from each player in result
         defer.resolve(data);
     });
 
     return defer.promise;
 };
 
-/* this method will provide more data that .locate */
-/* left join game data */
+/* this method provides more data that .locate */
 exports.stats = function(game, player) {
     var defer = $q.defer();
-    $db.get(game + '_players').findOne({name: new RegExp(player, 'i')}, function(err, player_data) {
+    $db.get(game + '_players').findOne({name: _sanitize(player)}, function(err, player_data) {
+        if (!player_data) return defer.reject();
+
+        /* left join last 20 games */
+        player_data.games = player_data.games.slice(-20);
         $db.get(game + '_games').find({idno: {$in: player_data.games}}, function(err, game_data) {
-            $db.get(game + '_ladder').findOne({name: player_data.name}, function(err, rank_data) {
-                if (rank_data && rank_data.rank) player_data.rank = rank_data.rank;
-
-                if (game_data.length < 1) defer.resolve(player_data);
-
+            if (game_data.length > 0) {
                 game_data.forEach(function(stats, index) {
                     player_data.games[index] = stats;
                 });
+            }
 
+            /* leaderboard position */
+            $db.get(game + '_ladder').findOne({name: player_data.name}, function(err, rank_data) {
+                player_data.rank = rank_data.rank || 0;
                 defer.resolve(player_data);
             });
         });
@@ -33,3 +37,7 @@ exports.stats = function(game, player) {
 
     return defer.promise;
 };
+
+function _sanitize(str) {
+    return new RegExp(str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"), 'i');
+}
