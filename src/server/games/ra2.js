3129b1c308f0e18bbd98a688c26aff8c103bc19c
Sean Wragg <Sean.Wragg@disney.com>
2015-07-12 11:55:56 -0400

    adding official maps for ts and ra2

---
+++ src/server/games/ra2.js
@@ -0,0 +1,49 @@
+var $db = require('../lib/mongo');
+var debug = require('debug')('wol:leaderboard');
+
+var official_maps = [
+    'Alaskan Oil Spill',
+    'Anytown, Amerika',
+    'A Path Beyond II',
+    'Arctic Circle',
+    'Bering Strait',
+    'Canyon Fodder',
+    'City Under Siege',
+    'Cold War',
+    'Country Swing',
+    'DC Uprising',
+    'DEFCON 6',
+    'Depth Charge',
+    'El Dorado',
+    'Golden State Fwy',
+    'Hammer and Sickle',
+    'Heartland',
+    'Heck Freezes Over',
+    'Invasion Confirmed',
+    'Isle of War',
+    'Lake Blitzen',
+    'Little Big Lake',
+    'Malibu Cliffs',
+    'May Day',
+    'Montana DMZ',
+    'Mount Olympus',
+    'mp24t2',
+    'Official Tournament Map',
+    'Official Tournament Map A',
+    'Official Tournament Map B',
+    'Paris Revisited',
+    'Pinch Point',
+    'Sinkhole',
+    'Snowball\'s Chance',
+    'Snow Valley',
+    'South Pacific',
+    'Sovereign Land',
+    'Stormy Weather',
+    'The Alamo',
+    'Urban Rush',
+    'Wild Animal Park'
+];
+
+exports.process = function(match) {
+    return;
+};
--- src/server/games/ts.js
+++ src/server/games/ts.js
@@ -1,7 +1,34 @@
 var $db = require('../lib/mongo');
 var debug = require('debug')('wol:leaderboard');
 
-var official_maps = ['Forest Fires', 'Terraces', 'Big Bluntz Green 4'];
+var official_maps = [
+    'A River Runs Near It',
+    'Casey\'s Canyon',
+    'Cliffs of Insanity',
+    'Desolation Redux',
+    'Forest Fires',
+    'Grand Canyon',
+    'Grassy Knoll',
+    'Hextreme!',
+    'Ice Cliffs',
+    'Limited Access',
+    'Night of the Mutants',
+    'Pentagram',
+    'Pit Or Plateau',
+    'Pockets',
+    'Seismic',
+    'Sinkholes',
+    'Storms',
+    'Stormy Valley',
+    'Super Bidgehead Redux',
+    'Tactical Opportunities',
+    'Terraces',
+    'The Ice Must Flow',
+    'Tiberium Garden Redux',
+    'Tread Lightly',
+    'Tunnel Training',
+    'Big Bluntz Green 4',
+];
 
 exports.process = function(match) {
     return;
