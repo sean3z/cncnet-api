3e0403625497b7a7541973d2eee02d99c2c9ae6c
sean3z <seanwragg@gmail.com>
2015-08-15 17:43:43 -0400

    add protection around search and increase games shown per player

--- src/server/lib/player.js
+++ src/server/lib/player.js
@@ -6,11 +6,13 @@ exports.locate = function(game, search) {
     var defer = $q.defer();
     $db.get(game + '_players').find({name: _sanitize(search)}, {limit: 10}, function(err, data) {
         /* remove games array from response */
-        data.forEach(function(item) {
-            delete item.games;
-        });
+        if (data && data.length > 0) {
+            data.forEach(function(item) {
+                delete item.games;
+            });
+        }
 
-        defer.resolve(data);
+        defer.resolve(data || []);
     });
 
     return defer.promise;
@@ -22,8 +24,8 @@ exports.stats = function(game, player) {
     $db.get(game + '_players').findOne({name: _sanitize(player)}, function(err, player_data) {
         if (!player_data) return defer.reject();
 
-        /* left join last 20 games */
-        player_data.games = player_data.games.slice(-20);
+        /* left join last 50 games */
+        player_data.games = player_data.games.slice(-50);
         $db.get(game + '_games').find({idno: {$in: player_data.games}}, function(err, game_data) {
             if (game_data && game_data.length > 0) {
                 game_data.forEach(function(stats, index) {
