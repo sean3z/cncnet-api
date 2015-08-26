1972f010c84ea461fcab2e0632cba8e79e8cb2cd
sean3z <seanwragg@gmail.com>
2015-08-25 20:42:34 -0400

    adding reassessment debug process

--- src/server/routes/debug.js
+++ src/server/routes/debug.js
@@ -1,15 +1,17 @@
 var debug = require('debug')('wol:leaderboard');
 var $db = require('../lib/mongo');
 var gameres = require('../lib/gameres');
+var match = require('../lib/match');
 
 exports.reset = function(req, res, next) {
     var games = ['ts', 'ra', 'td', 'd2', 'am', 'fs'];
     games.forEach(function(game) {
-        $db.get(game +'_dumps').drop();
+        // $db.get(game +'_dumps').drop();
         $db.get(game +'_games').drop();
         $db.get(game +'_players').drop();
-        $db.get(game +'_ladder').drop();
     });
+
+    global.cache = {};
 
     res.send(200);
 };
@@ -28,4 +30,47 @@ exports.gameres = function(req, res, next) {
 
 exports.submit = function(req, res, next) {
     res.send(gameres.parse(req.body));
+};
+
+exports.reassess = function(req, res, next) {
+    var $dumps = $db.get(req.params.game +'_dumps');
+    $dumps.find({}, function(err, data) {
+        if (!data || data.length < 1) return;
+
+        var i = 0;
+        var $old = $db.get(req.params.game +'_dumps_backup');
+        $old.drop();
+
+        (function _next() {
+            console.log('i', i);
+            if (i < data.length) {
+                $old.insert(data[i]).success(function() {
+                    _next(i++);
+                });
+            }
+
+            if (i >= data.length) {
+                $dumps.drop().success(function() {
+                    $old.find({}, function(err, data) {
+                        data.forEach(function(body) {
+                            match.process(req.params.game, body.buffers[0].buffer);
+                        });
+
+                        res.send({meow: 'cat'});
+                    });
+                });
+            }
+        })();
+    });
+
+        /*
+            $db.get(req.params.game +'_old').find({}, function(err, data) {
+        data.forEach(function(body) {
+            match.process(req.params.game, body.buffers[0].buffer);
+        });
+
+        res.send({meow: 'cat'});
+    });
+        */
+
 };
