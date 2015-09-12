105624aefd497318c63bf745c5ce8e0052c37415
sean3z <seanwragg@gmail.com>
2015-09-12 11:26:22 -0400

    loosen search params

--- src/server/lib/player.js
+++ src/server/lib/player.js
@@ -4,7 +4,7 @@ var $q = require('q');
 /* method to search for a quick list of players by name */
 exports.locate = function(game, search) {
     var defer = $q.defer();
-    $db.get(game + '_players').find({name: _sanitize(search)}, {limit: 10}, function(err, data) {
+    $db.get(game + '_players').find({name: _sanitize(search, false)}, {limit: 10}, function(err, data) {
         /* remove games array from response */
         if (data && data.length > 0) {
             data.forEach(function(item) {
@@ -21,7 +21,7 @@ exports.locate = function(game, search) {
 /* this method provides more data that .locate */
 exports.stats = function(game, player) {
     var defer = $q.defer();
-    $db.get(game + '_players').findOne({name: _sanitize(player)}, function(err, player_data) {
+    $db.get(game + '_players').findOne({name: _sanitize(player, true)}, function(err, player_data) {
         if (!player_data) return defer.reject();
 
         /* left join last 50 games */
@@ -50,7 +50,8 @@ exports.stats = function(game, player) {
     return defer.promise;
 };
 
-function _sanitize(str) {
-    var reg = '^'+ str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") +'$';
+function _sanitize(str, strict) {
+    var reg = str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    if (strict) reg = '^'+ reg +'$';
     return new RegExp(reg, 'i');
 }
