14f919161401c5297d03e72283f9c0988d1c4b68
sean3z <seanwragg@gmail.com>
2015-09-19 16:52:22 -0400

    assume player won if not dead, resigned or connection lost

--- src/server/lib/games/lib/ra.js
+++ src/server/lib/games/lib/ra.js
@@ -3,14 +3,13 @@ var debug = require('debug')('wol:leaderboard');
 /* brings ra1 gameres up to par */
 exports.normalize = function(match) {
     (match.players || []).forEach(function(player) {
+        player.cmp = 256;
         if (player.ded) {
             player.cmp = 528;
         } else if (player.rsg) {
             player.cmp = 512;
         } else if (player.con) {
             player.cmp = 2;
-        } else {
-            player.cmp = 256;
         }
     });
 
