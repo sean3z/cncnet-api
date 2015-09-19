9b9494227cbe0b1720bd3895a5366052af138889
sean3z <seanwragg@gmail.com>
2015-09-19 16:33:42 -0400

    resiliency

---
+++ src/server/routes/ladder.js
@@ -0,0 +1,42 @@
+var match = require('../lib/match');
+var ranking = require('../lib/ranking');
+var player = require('../lib/player');
+var debug = require('debug')('wol:leaderboard');
+
+exports.submit = function (req, res, next) {
+    res.send(202);
+    match.process(req.params.game, req.body);
+};
+
+exports.ladder = function (req, res, next) {
+    ranking.ladder(req.params.game, 150).then(function(data) {
+        res.send(data);
+    });
+};
+
+exports.search = function (req, res, next) {
+    if (!req.body.player) return _error();
+    player.locate(req.params.game, req.body.player).then(res.send, _error);
+
+    var _error = function () {
+        res.send(404);
+    };
+};
+
+exports.player = function(req, res, next) {
+    if (!req.params.player) return _error();
+    player.stats(req.params.game, req.params.player).then(res.send, _error);
+
+    var _error = function () {
+        res.send(404);
+    };
+};
+
+exports.match = function(req, res, next) {
+    if (!req.params.gameId) return _error();
+    match.information(req.params.game, parseInt(req.params.gameId)).then(res.send, _error);
+
+    var _error = function () {
+        res.send(404);
+    };
+};
