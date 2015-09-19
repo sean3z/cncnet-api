0e298d825512a29326ec150d46c4810cec323578
sean3z <seanwragg@gmail.com>
2015-09-19 16:34:26 -0400

    leaderboard -> ladder

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ global.cwd = __dirname;
 
 /* route separation http://bit.ly/1Kt87xZ */
 var ping = require('./routes/ping');
-var leaderboard = require('./routes/leaderboard');
+var ladder = require('./routes/ladder');
 var auth = require('./routes/auth');
 var debug = require('./routes/debug');
 
@@ -38,11 +38,11 @@ app.use(function(req, res, next) {
 app.get('/ping', ping);
 
 /* leaderboard */
-app.post('/ladder/:game', leaderboard.submit);
-app.get('/ladder/:game', leaderboard.ladder);
-app.get('/ladder/:game/game/:gameId', leaderboard.match);
-app.get('/ladder/:game/player/:player', leaderboard.player);
-app.post('/ladder/:game/search', leaderboard.search);
+app.post('/ladder/:game', ladder.submit);
+app.get('/ladder/:game', ladder.ladder);
+app.get('/ladder/:game/game/:gameId', ladder.match);
+app.get('/ladder/:game/player/:player', ladder.player);
+app.post('/ladder/:game/search', ladder.search);
 
 /* auth */
 app.get('/auth/:game/:player', auth.player);
@@ -53,7 +53,6 @@ app.get('/debug/reset', debug.reset);
 app.get('/debug/buffer/:game/:gameId', debug.buffer);
 app.get('/debug/gameres/:game/:gameId', debug.gameres);
 app.post('/debug/gameres/:game', debug.submit);
-app.get('/debug/reassess/:game', debug.reassess);
 
 /* static server for development */
 app.get(/.*/, restify.serveStatic({
