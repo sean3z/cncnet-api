17257d76ad2a779e686b7830fd4c6d1d201c72e4
sean3z <seanwragg@gmail.com>
2015-09-19 19:21:05 -0400

    cleaning up

--- src/server/lib/match/process.js
+++ src/server/lib/match/process.js
@@ -6,7 +6,7 @@ var ranking = require('../ranking');
 module.exports = function process(game, dmp) {
     var match = require(__dirname + '/lib/parse')(game, gameres.parse(dmp));
 
-    /* discontinue if no game id or match is less than 1 minute */
+    /* discontinue if no gameId or match is less than 1 minute */
     if (!match.idno || match.dura < 60) return;
     debug('game: %s, idno: %d', game, match.idno);
 
@@ -14,12 +14,7 @@ module.exports = function process(game, dmp) {
     // TODO: check against spid (sender id) to ensure only 1 packet from each player
     // TODO: idno is random but could collide. check if record exists and compare dates
     var $dumps = $db.get(game +'_dumps');
-    $dumps.update(
-        {idno: match.idno},
-        {$push: {buffers: match.buffer}},
-        {upsert: true},
-        _stats
-    );
+    $dumps.update({idno: match.idno}, {$push: {buffers: match.buffer}}, {upsert: true}, _stats);
 
     function _stats(err, data) {
         $dumps.find({idno: match.idno}, function(err, doc) {
@@ -34,12 +29,11 @@ module.exports = function process(game, dmp) {
             // only continue if this is the first entry for a game
             if (doc[0].buffers && doc[0].buffers.length > 1) return;
 
-            /* save game stats */
+            /* save match */
             $db.get(game +'_games').insert(match).success(function(doc) {
                 ranking.process(game, match);
                 debug('game: %s, idno: %d saved!', game, match.idno);
             });
-
         });
     }
 
