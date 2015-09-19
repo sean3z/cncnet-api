25209909379f8741e97b7c18d7ee029742e5d7f0
sean3z <seanwragg@gmail.com>
2015-09-19 17:21:03 -0400

    exporting function

--- src/server/lib/ranking/process.js
+++ src/server/lib/ranking/process.js
@@ -1,6 +1,6 @@
 var debug = require('debug')('wol:leaderboard');
 
-exports.process = function(game, match) {
+module.exports = function process(game, match) {
     /* handle elo only for singles games */
     if (match.players.length == 2) {
         debug('game: %s, idno: %d is singles', game, match.idno);
