6c7e23644e1708eaf212f6e2411779529b41530e
sean3z <seanwragg@gmail.com>
2015-09-23 21:01:14 -0400

    fixing typo

--- src/server/lib/mongo.js
+++ src/server/lib/mongo.js
@@ -4,7 +4,7 @@ var games = require('./games');
 games.supported.forEach(function(game) {
     $db.get(game +'_dumps').index('idno', {unique: true});
     $db.get(game +'_players').index('name', {unique: true});
-    $db.get(game +'_game').index('idno', {unique: true});
+    $db.get(game +'_games').index('idno', {unique: true});
 });
 
 module.exports = $db;
