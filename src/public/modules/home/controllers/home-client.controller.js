fa39f3403a468f03cb590b4e0bc05c7c3b25ceb4
GrantBartlett <hello@grant-bartlett.com>
2015-10-25 18:59:07 +0000

    Index page

--- src/public/modules/home/controllers/home-client.controller.js
+++ src/public/modules/home/controllers/home-client.controller.js
@@ -2,54 +2,22 @@
 
 // Ladder controller
 angular.module('index').controller('HomeController', ['$scope', '$state', '$stateParams', 'LadderSvc', 'PlayerSearch',
-    function ($scope, $state, $stateParams, LadderSvc, PlayerSearch) {
+    function ($scope, $state, $stateParams, LadderSvc) {
 
-        LadderSvc.getTop50($stateParams.game).then(function(response) {
-            $scope.players = response;
-        });
+        // Its a little hacky, but will do for now
+        var games = ['ts', 'fs', 'ra', 'am'];
 
-        // Search by player name
-        $scope.searchByPlayer = function(){
-            if($stateParams.game){
-                PlayerSearch.byPlayer($stateParams.game, $scope.search.name).then(function (response) {
-                    $scope.searchResults = response;
-                });
-            }
+        $scope.top5 = {
+            ts: '',
+            fs: '',
+            ra: '',
+            am: ''
         };
 
-        // Until we have game names in our response
-        switch($stateParams.game) {
-            case 'ts':
-                $scope.abbr = 'ts';
-                $scope.game = 'Tiberian Sun';
-                break;
-            case 'ra':
-                $scope.abbr = 'ra';
-                $scope.game = 'Red Alert';
-                break;
-            case 'yr':
-                $scope.abbr = 'yr';
-                $scope.game = 'Yuri\'s Revenge';
-                break;
-            case 'fs':
-                $scope.abbr = 'fs';
-                $scope.game = 'Firestorm';
-                break;
-            case 'am':
-                $scope.abbr = 'am';
-                $scope.game = 'Aftermath';
-                break;
-            case 'ra2':
-                $scope.abbr = 'ra2';
-                $scope.game = 'Red Alert 2';
-                break;
-            case 'dta':
-                $scope.abbr = 'dta';
-                $scope.game = 'The Dawn of the Tiberium Age';
-                break;
-            default:
-                $scope.game = 'Global';
-        }
+        games.forEach(function (key) {
+            LadderSvc.getTop50(key).then(function (response) {
+                $scope.top5[key] = response;
+            });
+        });
     }
 ]);
-
