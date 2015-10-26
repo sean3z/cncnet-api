8aea32cfbafcaee4ace174305681cf878e227d1e
sean <seanwragg@gmail.com>
2015-10-26 07:31:12 -0400

    converting all nicks to lowercase

--- src/server/lib/player/lib/sanitize.js
+++ src/server/lib/player/lib/sanitize.js
@@ -1,5 +1,5 @@
 module.exports = function _sanitize(str, strict) {
-    var reg = str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    var reg = str.toLowerCase().replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
     if (strict) reg = '^' + reg + '$';
     return new RegExp(reg, 'i');
 };
