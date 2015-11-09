14042ee91592b1ff425c09f8f603b5eaba69c351
GrantBartlett <hello@grant-bartlett.com>
2015-11-09 18:56:34 +0000

    Prep for factions but also compressed jumbos from to below <500kb

--- dist/public/application.js
+++ dist/public/application.js
@@ -92,14 +92,11 @@ angular.module('index')
 angular.module('Leaderboard.Player').controller('LeaderboardPlayerController', ['$scope', '$state', '$stateParams', 'LadderPlayerSvc',
     function ($scope, $state, $stateParams, LadderPlayerSvc) {
 
-        console.log('hi');
         $scope.gameName = $stateParams.game;
 
         LadderPlayerSvc.getPlayer($stateParams.game, $stateParams.player).then(function (response) {
            $scope.player = response;
         });
-
-        console.log($stateParams);
 
     }
 ]);
--- src/public/modules/leaderboard-player/controllers/leaderboard-player.client.controller.js
+++ src/public/modules/leaderboard-player/controllers/leaderboard-player.client.controller.js
@@ -4,14 +4,11 @@
 angular.module('Leaderboard.Player').controller('LeaderboardPlayerController', ['$scope', '$state', '$stateParams', 'LadderPlayerSvc',
     function ($scope, $state, $stateParams, LadderPlayerSvc) {
 
-        console.log('hi');
         $scope.gameName = $stateParams.game;
 
         LadderPlayerSvc.getPlayer($stateParams.game, $stateParams.player).then(function (response) {
            $scope.player = response;
         });
-
-        console.log($stateParams);
 
     }
 ]);
