78605a1cd233e6127f7fcbb71204291880d8011d
sean <seanwragg@gmail.com>
2015-11-18 21:10:23 -0500

    adding RA test

--- test/server/functional/ladder.spec.js
+++ test/server/functional/ladder.spec.js
@@ -3,7 +3,55 @@ var request = require('request');
 var MATCH_DELAY = process.env.MATCH_DELAY + 10;
 
 describe('Ladder Endpoints', function() {
-    it('Regular match: 2 packets', function(done) {
+    it('RA Regular match: 2 packets', function(done) {
+        var robskate = fs.readFileSync(scenarios + '/RA_REGULAR_PLAYER_2');
+
+        var options = {
+            method: 'POST',
+            url: url + '/ladder/ra',
+            body: robskate.toString(),
+            headers: {
+                'content-type': 'text/plain',
+                authorization: 'Basic dGFoajpwYXNzd29yZA=='
+            }
+        };
+
+        request(options, function(err, res) {
+            expect(res.statusCode).to.equal(202);
+        });
+
+        var fattynoob = fs.readFileSync(scenarios + '/RA_REGULAR_PLAYER_1');
+        options.body = fattynoob.toString();
+
+        request(options, function(err, res) {
+            expect(res.statusCode).to.equal(202);
+        });
+
+        setTimeout(function() {
+            request({url: url + '/ladder/ra/player/fattynoob'}, function(err, res, body) {
+                expect(res.statusCode).to.equal(200);
+                body = JSON.parse(body);
+                expect(body.wins).to.equal(0);
+                expect(body.losses).to.equal(1);
+                expect(body.disconnects).to.equal(0);
+                expect(body.games.length).to.equal(1);
+                expect(body.points).to.equal(984);
+            });
+
+            request({url: url + '/ladder/ra/player/robskate'}, function(err, res, body) {
+                expect(res.statusCode).to.equal(200);
+                body = JSON.parse(body);
+                expect(body.wins).to.equal(1);
+                expect(body.losses).to.equal(0);
+                expect(body.disconnects).to.equal(0);
+                expect(body.games.length).to.equal(1);
+                expect(body.points).to.equal(1016);
+                done();
+            });
+        }, MATCH_DELAY);
+    });
+
+    it('TS Regular match: 2 packets', function(done) {
         var gameranger = fs.readFileSync(scenarios + '/TS_REGULAR_PLAYER_2');
 
         var options = {
@@ -51,7 +99,7 @@ describe('Ladder Endpoints', function() {
         }, MATCH_DELAY);
     });
 
-    it('D/C Scenario: 1 packet declare uploader winner', function(done) {
+    it('TS D/C Scenario: 1 packet declare uploader winner', function(done) {
         var results = fs.readFileSync(scenarios + '/TS_DISCONNECT_PLAYER_1');
 
         var options = {
@@ -92,7 +140,7 @@ describe('Ladder Endpoints', function() {
         });
     });
 
-    it('should provide leaderboard for players', function(done) {
+    it('should provide TS leaderboard for players', function(done) {
         /* warm up cache */
         request(url + '/ladder/ts', function() {});
 
