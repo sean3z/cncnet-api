6b15d96d8b192f289c8bebfce9d54563d54d2ca3
sean <seanwragg@gmail.com>
2015-11-18 20:58:32 -0500

    protecting match processing higher up the chain

--- src/server/lib/match/process.js
+++ src/server/lib/match/process.js
@@ -14,43 +14,33 @@ module.exports = function process(game, dump) {
     // TODO: check against spid (sender id) to ensure only 1 packet from each player
     // TODO: idno is random but could collide. check if record exists and compare dates
     var $dumps = $db.get(game +'_dumps');
-    $dumps.update({idno: match.idno}, {$push: {buffers: match.buffer}}, {upsert: true}, _stats).error(function(err) {
+    $dumps.update({idno: match.idno}, {$push: {buffers: match.buffer}}, {upsert: true}).error(function(err) {
         console.log('match/process dump entry error');
         console.log('game: %s, match: %d', game, match.idno);
         console.log('buffer: %s', match.buffer.toString('hex'));
         console.dir(err);
     });
 
-    function _stats(err, data) {
-        if (err) throw err;
-        $dumps.find({idno: match.idno}, function(err, doc) {
-            if (doc.length > 1) {
-                // error scneario, we have two entries for the same game
-                // todo: figure out what the hell to do?
-                // todo: elimiate duplicates?
-                debug('game: %s, idno: %d found duplicates!', game, match.idno);
-                return;
-            }
+    // remove unused properties from game object
+    delete match.buffer; /* only used for previous parsing */
+    delete match.client; /* unused information about the client */
 
-            // only continue if this is the first entry for a game
-            if (doc[0].buffers && doc[0].buffers.length > 1) return;
+    /* save match */
+    var p = $db.get(game +'_games').insert(match);
+    p.success(function(doc) {
+        debug('game: %s, idno: %d game saved!', game, match.idno);
 
-            // remove unused properties from game object
-            delete match.buffer; /* only used for previous parsing */
-            delete match.client; /* unused information about the client */
+        /* process rankings at a 1.5 minute delay */
+        /* this allows time for all packets to arrive */
+        setTimeout(function() {
+            ranking.process(game, match);
+        }, MATCH_DELAY);
+    });
 
-            /* save match */
-            $db.get(game +'_games').insert(match).success(function(doc) {
-                debug('game: %s, idno: %d game saved!', game, match.idno);
-
-                /* process rankings at a 1.5 minute delay */
-                /* this allows time for all packets to arrive */
-                setTimeout(function() {
-                    ranking.process(game, match);
-                }, MATCH_DELAY);
-            });
-        });
-    }
+    p.error(function(err) {
+        console.log('error');
+        console.dir(err);
+    });
 
     return;
 };
