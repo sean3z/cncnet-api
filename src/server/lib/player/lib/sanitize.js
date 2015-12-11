71aabccd82b8a8367310e607798b681ac62da4d7
sean <seanwragg@gmail.com>
2015-12-10 23:15:48 -0500

    adding flexibility to sanatize player without returning regex

--- src/server/lib/player/lib/sanitize.js
+++ src/server/lib/player/lib/sanitize.js
@@ -1,5 +1,8 @@
-module.exports = function _sanitize(str, strict) {
+module.exports = function _sanitize(str, strict, repl) {
     var reg = str.toLowerCase().replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-    if (strict) reg = '^' + reg + '$';
-    return new RegExp(reg);
+    if (!repl) {
+        if (strict) reg = '^' + reg + '$';
+        return new RegExp(reg);
+    }
+    return reg;
 };
