38d91c1bbc0a961afb397b8e536fe6e8c308456f
Sean Wragg <Sean.Wragg@disney.com>
2016-06-30 11:42:44 -0400

    removing ui

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,14 @@
 module.exports = function (grunt) {
     require('jit-grunt')(grunt);
 
-    var config = {
-        modules: {
-            js: [
-                'src/public/app.js',
-                'src/public/modules/**/*.client.module.js',
-                'src/public/modules/**/*.js'
-            ]
-        }
-    };
-
     grunt.initConfig({
-        clean: ['dist'],
-
-        copy: {
-            development: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/public',
-                    src: ['**', '!**/sass/**'],
-                    dest: 'dist/public/'
-                }]
-            }
-        },
-
-        sass: {
-            development: {
-                options: {
-                    sourcemap: 'none',
-                    style: 'compact'
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'src/public/assets/sass',
-                    src: ['*.scss'],
-                    dest: 'dist/public/assets/css',
-                    ext: '.css'
-                }]
-            }
-        },
-
-        watch: {
-            public: {
-                files: ['src/public/**'],
-                tasks: ['copy', 'sass:development']
-            }
-        },
-
         nodemon: {
             development: {
                 script: 'src/server',
                 options: {
-                    ignore: ['src/public'],
                     env: {
                         'NODE_ENV': 'development'
                     }
-                }
-            }
-        },
-
-        ngAnnotate: {
-            production: {
-                files: {
-                    'src/public/application.js': config.modules.js
-                }
-            }
-        },
-
-        concurrent: {
-            development: {
-                tasks: ['watch:public', 'nodemon:development', 'ngAnnotate'],
-                options: {
-                    logConcurrentOutput: true
                 }
             }
         },
@@ -88,10 +24,6 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-ng-annotate');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-
-    grunt.registerTask('serve', ['clean', 'copy', 'sass:development', 'concurrent:development']);
-    grunt.registerTask('build', ['clean', 'copy', 'sass:development', 'ngAnnotate']);
+    grunt.registerTask('serve', ['nodemon:development']);
     grunt.registerTask('test', ['mocha_istanbul:server'])
 };
--- src/public/modules/champions/champions.module.js
+++
@@ -1,1 +0,0 @@
-angular.module('Champions', ['ui.router']);
